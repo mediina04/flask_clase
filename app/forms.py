9c955c58390be37fc3738b54f3781739a3f97b13
Marc Santiago <marcsantiagofp@ibf.cat>
2025-01-13 17:00:35 +0100

    modified:   app/__pycache__/forms.cpython-312.pyc 	modified:   app/forms.py 	modified:   app/templates/base.html 	modified:   app/templates/information.html 	modified:   main.py

--- app/forms.py
+++ app/forms.py
@@ -2,13 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
-# Formulario para el inicio de sesión
+# Formulario de login
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Nom d\'usuari', validators=[DataRequired()])
     password = PasswordField('Contrasenya', validators=[DataRequired()])
     submit = SubmitField('Iniciar sessió')
 
-# Formulario para el registro de usuario
+# Formulario de registro
 class RegisterForm(FlaskForm):
     username = StringField('Nom d\'usuari', validators=[DataRequired(), Length(min=4, max=100)])
     password = PasswordField('Contrasenya', validators=[DataRequired(), Length(min=6)])
